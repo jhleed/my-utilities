65a591422c6be629e2755664a218dcde05eab3d9
jongholee <jhleed3@gmail.com>
2019-05-04 16:10:22 +0900

    블로그 포스팅 스크립트 추가

--- Bloging/upload.sh
+++ Bloging/upload.sh
@@ -4,6 +4,6 @@ unzip *
 
 markdown-tistory write
 
-rm -rf *
+rm -rf /Users/jongholee/dev/workspace/my-utilities/Bloging/upload/*
 
 /usr/bin/open -a "/Applications/Google Chrome.app" 'https://jhleed.tistory.com/'
